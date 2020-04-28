70478e2aed70fbaca84ccb271e2223cf5868b480
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-04-29 01:11:26 +0530

    producer consumer using wait and notify

---
+++ src/tutorial9/demo1/App.java
@@ -0,0 +1,37 @@
+package tutorial9.demo1;
+
+public class App {
+
+	public static void main(String[] args) throws InterruptedException {
+		final Processor process = new Processor();
+
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					process.produce();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				try {
+					process.consume();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+	}
+
+}
